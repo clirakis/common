3d18ae90897f6162ad6e87fdf96c29e745d7181a
Chris Lirakis <clirakis@gmail.com>
2025-11-04 17:29:41 -0500

    Now has capability to do an ostream and also produce a NMEA VTG string.

--- libNMEA/VTG.cpp
+++ libNMEA/VTG.cpp
@@ -13,6 +13,35 @@
  * Classification : Unclassified
  *
  * References :
+ * https://receiverhelp.trimble.com/alloy-gnss/en-us/nmea0183-messages-vtg.html?Highlight=VTG
+ * https://docs.fixposition.com/fd/nmea-gp-vtg
+ *
+ * From fixposition site:
+ *    $GNVTG,0.0000,T,,M,0.01316,N,0.02437,K,D*3F\r\n
+ *
+ * From GTOP:
+ *    $GPVTG,12.71,T,,M,0.10,N,0.18,K,D*05
+ *
+ * Records
+ * 0 - Message ID
+ * 1 - Track made good, degrees true
+ * 2 T: trck made good relative to true north
+ * 3 Track made good - magnetic
+ * 4 M - specify magnetic north
+ * 5 Speed in knots
+ * 6 N - specifying knots
+ * 7 Speed over ground in kph
+ * 8 K - specifying that KPH
+ * 9 Mode:
+ *    A: Autonomous
+ *    D: Differential
+ *    E: Estimated or Dead reconing
+ *    M: Manual input
+ *    S: Simulator
+ *    N: Invalid data. 
+ *    
+ * 10 Checksum
+ *    
  *
  ********************************************************************/
 // System includes.
@@ -26,12 +55,14 @@ using namespace std;
 // Local Includes.
 #include "debug.h"
 #include "VTG.hh"
+#include "NMEA_helper.hh"
+#include "tools.h"
 
 
 /**
  ******************************************************************
  *
- * Function Name :  
+ * Function Name :  VTG constructor
  *
  * Description : 
  *
@@ -41,7 +72,7 @@ using namespace std;
  *
  * Error Conditions : none
  *
- * Unit Tested on:
+ * Unit Tested on: 05-Nov-25
  *
  * Unit Tested by: CBL
  *
@@ -55,7 +86,7 @@ VTG::VTG(void)
     fMagnetic   = 0.0;  // course magnetic
     fSpeedKnots = 0.0;
     fSpeedKPH   = 0.0; 
-    fMode       = 'N';
+    fMode       = 'N';  // Data no set
     SET_DEBUG_STACK;
 }
 
@@ -72,7 +103,7 @@ VTG::VTG(void)
  *
  * Error Conditions :
  * 
- * Unit Tested on: 
+ * Unit Tested on: 05-Nov-25
  *
  * Unit Tested by: CBL
  *
@@ -81,12 +112,13 @@ VTG::VTG(void)
  */
 VTG::~VTG (void)
 {
+    // Nothing to be done
 }
 
 /**
  ******************************************************************
  *
- * Function Name : VTG function
+ * Function Name : VTG Decode given a string
  *
  * Description :
  *
@@ -109,14 +141,14 @@ bool VTG::Decode(const char *line)
     char *p = (char *) line;
 
     p = strchr(p, ',')+1;
-    fTrue = atof(p);      // Course True
+    fTrue = atof(p) * DegToRad;      // Course True, stored as radians
 
     p = strchr(p, ',')+1;
     if (*p != 'T')
 	return false;
 
     p = strchr(p, ',')+1;
-    fMagnetic = atof(p);  // course magnetic
+    fMagnetic = atof(p) * DegToRad;  // course magnetic, stored as radians
 
     p = strchr(p, ',')+1;
     if (*p != 'M')
@@ -140,4 +172,197 @@ bool VTG::Decode(const char *line)
     fMode = *p;
     SET_DEBUG_STACK;
     return true;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : Encode
+ *
+ * Description : Make a NMEA VTG string
+ *
+ * Inputs : NONE
+ *
+ * Returns : string with NMEA VTG message 
+ *
+ * Error Conditions : None
+ * 
+ * Unit Tested on: 04-Nov-25
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+string VTG::Encode(void)
+{
+    SET_DEBUG_STACK;
+
+    char txt[16];
+    string rv("$GPVTG,");
+
+    snprintf(txt,sizeof(txt),"%5.2f,T,", fTrue );
+    rv += txt;
+    snprintf(txt,sizeof(txt),"%5.2f,M,", fMagnetic);
+    rv += txt;
+    snprintf(txt,sizeof(txt),"%4.2f,N,", fSpeedKnots);
+    rv += txt;
+    snprintf(txt,sizeof(txt),"%4.2f,K,", fSpeedKPH);
+    rv += txt;
+    switch(fMode)
+    {
+    case kAUTONOMOUS:
+	rv += "A*";
+	break;
+    case kDIFFERENTIAL:
+	rv += "D*";
+	break;
+    case kDR:
+	rv += "E*";
+	break;
+    case kMANUAL:
+	rv += "M*";
+	break;
+    case kSIMULATION:
+	rv += "S*";
+	break;
+    case kNONE:
+    default:
+	rv += "N*";
+	break;
+    }
+    snprintf(txt, sizeof(txt), "%2.2X", Checksum(rv));
+    rv += txt;
+    return rv;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : operator<< for VTG
+ *
+ * Description : formats the data from the VTG class
+ *
+ * Inputs : VTG class. 
+ *
+ * Returns :
+ *
+ * Error Conditions :
+ * 
+ * Unit Tested on: 04-Nov-25
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+ostream& operator<<(ostream& output, const VTG &n)
+{
+    SET_DEBUG_STACK;
+    output << "VTG::" << endl
+	   << "         COG: " << n.fTrue * RadToDeg << endl
+	   << "    Magnetic: " << n.fMagnetic * RadToDeg << endl
+	   << "    Speed(K): " << n.fSpeedKnots << endl
+	   << "  Speed(kPH): " << n.fSpeedKPH << endl
+	   << "        Mode: " << n.ModeStr() << endl;
+    return output;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : SetKnots
+ *
+ * Description : Set the speed in knots. Since the speeds are related
+ *               sets KPH at the same time. 
+ *
+ * Inputs : v - speed in knots
+ *
+ * Returns : none
+ *
+ * Error Conditions : none
+ * 
+ * Unit Tested on: 04-Nov-25
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+void VTG::SetKnots(float v)
+{
+    SET_DEBUG_STACK;
+    fSpeedKnots = v;
+    fSpeedKPH   = 1.852*v;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : SetKPH
+ *
+ * Description : Sets KPH, but also updates Knots field
+ *
+ * Inputs : v - KPH value to update to
+ *
+ * Returns : NONE
+ *
+ * Error Conditions : NONE
+ * 
+ * Unit Tested on: 04-Nov-25
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+void VTG::SetKPH(float v)
+{
+    SET_DEBUG_STACK;
+    fSpeedKPH = v;
+    fSpeedKnots = v/1.852;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : ModeStr
+ *
+ * Description : Give a string descriptor for the current mode used.
+ *
+ * Inputs : NONE
+ *
+ * Returns : NONE
+ *
+ * Error Conditions : NONE
+ * 
+ * Unit Tested on:  04-Nov-25
+ *
+ * Unit Tested by: CBL
+ *
+ *
+ *******************************************************************
+ */
+string VTG::ModeStr(void) const
+{
+    SET_DEBUG_STACK;
+    string rv;
+    switch(fMode)
+    {
+    case kAUTONOMOUS:
+	rv = "Autonomous";
+	break;
+    case kDIFFERENTIAL:
+	rv = "Differential";
+	break;
+    case kDR:
+	rv = "Dead reconing";
+	break;
+    case kMANUAL:
+	rv = "Manual";
+	break;
+    case kSIMULATION:
+	rv = "Simulation";
+	break;
+    case kNONE:
+    default:
+	rv = "NONE or Data not valid";
+	break;
+    }
+    return rv;
 }
--- libNMEA/VTG.hh
+++ libNMEA/VTG.hh
@@ -14,6 +14,8 @@
  * Classification : Unclassified
  *
  * References :
+ * https://receiverhelp.trimble.com/alloy-gnss/en-us/nmea0183-messages-vtg.html?Highlight=VTG
+ * https://docs.fixposition.com/fd/nmea-gp-vtg
  *
  *******************************************************************
  */
@@ -24,15 +26,24 @@
 class VTG
 {
 public:
+    /*!
+     * Constructor for NMEA VTG message. 
+     */
     VTG(void);
     ~VTG(void);
+    enum MODES{kAUTONOMOUS=0, kDIFFERENTIAL, kDR, kMANUAL, kSIMULATION, kNONE};
 
     bool Decode(const char *);
     inline float True(void)  const {return fTrue;};
+    inline void  SetTrue(float v)  {fTrue = v;};
     inline float Mag(void)   const {return fMagnetic;};
+    inline void  SetMag(float v)   {fMagnetic = v;};
     inline float Knots(void) const {return fSpeedKnots;};
+    void  SetKnots(float v);
     inline float KPH(void)   const {return fSpeedKPH;};
+    void  SetKPH(float v);
     inline int   Mode(void)  const {return (int) fMode;};
+    inline void  SetMode(MODES v)  { fMode = v;};
 
     /*! Get a pointer to the beginning of the data storage. */
     inline void* DataPointer(void) {return (void*)&fTrue;};
@@ -41,18 +52,23 @@ public:
     inline static size_t DataSize(void) 
 	{return (4*sizeof(float)+4*sizeof(char));};
 
+    string Encode(void);
+    std::string ModeStr(void) const;
+
     /*! operator overload to output contents of class for inspection
      * this data is in character format. 
      */
     friend ostream& operator<<(ostream& output, const VTG &n); 
 
 private:
-    float fTrue;      // Course True
-    float fMagnetic;  // course magnetic
+
+    float fTrue;      // Course True - Radians
+    float fMagnetic;  // course magnetic - Radians
     float fSpeedKnots;
     float fSpeedKPH; 
     char  fMode;      // Autonomous, Differential, Estimated
     char  fSpace[3];  // round out to even 4 byte boundry
+
 };
 
 
