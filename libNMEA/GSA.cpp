f0cf8104cdd7cd0fb4faca6618c4e8e62377c282
Chris Lirakis <clirakis@gmail.com>
2025-11-04 20:24:27 -0500

    Added in Encode function for NMEA string.

--- libNMEA/GSA.cpp
+++ libNMEA/GSA.cpp
@@ -13,6 +13,8 @@
  * Classification : Unclassified
  *
  * References :
+ * https://docs.fixposition.com/fd/nmea-gp-gsa
+ * https://receiverhelp.trimble.com/alloy-gnss/en-us/nmea0183-messages-gsa.html?tocpath=Output%20Messages%7CNMEA-0183%20messages%7C_____8
  *
  ********************************************************************/
 // System includes.
@@ -26,15 +28,16 @@ using namespace std;
 // Local Includes.
 #include "debug.h"
 #include "GSA.hh"
+#include "NMEA_helper.hh"
 
 /**
  ******************************************************************
  *
- * Function Name : 
+ * Function Name : GSA constructor
  *
  * Description :
  *
- * Inputs :
+ * Inputs : NONE
  *
  * Returns :
  *
@@ -58,13 +61,13 @@ GSA::GSA(void)
 /**
  ******************************************************************
  *
- * Function Name : 
+ * Function Name : Decode
  *
- * Description :
+ * Description : Given a NMEA string, extract the constants
  *
- * Inputs :
+ * Inputs : character string containing NMEA GSA message
  *
- * Returns :
+ * Returns : True on success
  *
  * Error Conditions :
  * 
@@ -106,7 +109,7 @@ bool GSA::Decode(const char *line)
 /**
  ******************************************************************
  *
- * Function Name : 
+ * Function Name : operator << overload for GSA
  *
  * Description :
  *
@@ -116,7 +119,7 @@ bool GSA::Decode(const char *line)
  *
  * Error Conditions :
  * 
- * Unit Tested on: 
+ * Unit Tested on: 04-Nov-25
  *
  * Unit Tested by: CBL
  *
@@ -130,7 +133,7 @@ ostream& operator<<(ostream& output, const GSA &n)
 	   << "  HDOP: " << n.fHDOP << endl
 	   << "  VDOP: " << n.fVDOP << endl
 	   << " Mode1: " << n.fMode1 << endl
-	   << " Mode2: " << n.fMode2 << endl;
+	   << " Mode2: " << (int) n.fMode2 << endl;
     for (int i=0;i<12;i++)
     {
 	output << (unsigned int) n.fSatellite[i] << " ";
@@ -138,4 +141,56 @@ ostream& operator<<(ostream& output, const GSA &n)
     output << endl
 	   << " ========================================= " << endl;
     return output;
+}
+/**
+ ******************************************************************
+ *
+ * Function Name : Encode
+ *
+ * Description : Make a NMEA GSA string
+ *
+ * Inputs : NONE
+ *
+ * Returns : std::string GSA message
+ *
+ * Error Conditions :
+ * 
+ * Unit Tested on: 
+ *
+ * Unit Tested by: CBL
+ *
+ * example from GTOP receiver
+ *
+ * $GPGSA,A,3,30,19,10,07,13,22,01,08,17,02,14,,1.18,0.88,0.79*03
+ *
+ *******************************************************************
+ */
+string GSA::Encode(void)
+{
+    SET_DEBUG_STACK;
+    char txt[16];
+
+    string rv("$GPGSA,");
+    rv = rv + fMode1 + ",";
+    snprintf(txt,sizeof(txt),"%1.1d,", fMode2);
+    rv += txt;
+
+    for(int i=0;i<12;i++)
+    {
+	snprintf(txt, sizeof(txt), "%2.2d,", fSatellite[i]);
+	rv += txt;
+    }
+
+    snprintf(txt,sizeof(txt),"%4.2f,",fPDOP);
+    rv += txt;
+
+    snprintf(txt,sizeof(txt),"%4.2f,",fHDOP);
+    rv += txt;
+
+    snprintf(txt,sizeof(txt),"%4.2f*",fVDOP);
+    rv += txt;
+
+    snprintf(txt,sizeof(txt),"%2.2X",Checksum(rv));
+    rv += txt;
+    return rv;
 }
--- libNMEA/GSA.hh
+++ libNMEA/GSA.hh
@@ -24,15 +24,49 @@
 class GSA 
 {
 public:
+
     GSA(void);
     bool Decode(const char *);
+    /*!
+     * Mode1  (M)anual or (A)utomatic.
+     */
     inline char Mode1(void) const {return fMode1;};
+    void SetMode1(char c) {if ((c=='M') || (c=='A')) fMode1 = c;};
+    /*!
+     * Mode 2 Fix Type:
+     *     1 - not available
+     *     2 - 2D
+     *     3 - 3D
+     */
     inline char Mode2(void) const {return fMode2;};
+    inline void SetMode2(char c) {if((c>0) && (c<4)) fMode2 = c;};
+    /*!
+     * for satellite i, return the numerical id. 
+     */
     inline unsigned char ID(int i) const {return fSatellite[i%12];};
+    inline void SetID(int i, char ID) {fSatellite[i%12]=ID;};
+    /*!
+     * Get the full ID array!
+     */
     inline const unsigned char* IDS(void) {return fSatellite;};
+    /*!
+     * get the PDOP
+     */
     inline float PDOP(void) const {return fPDOP;};
+    inline void  SetPDOP(float v) {fPDOP = v;};
+    /*!
+     * get the HDOP
+     */
     inline float HDOP(void) const {return fHDOP;};
+    inline void  SetHDOP(float v) {fHDOP = v;};
+    /*!
+     * Get the VDOP
+     */
     inline float VDOP(void) const {return fVDOP;};
+    inline void  SetVDOP(float v) {fVDOP = v;};
+    /*!
+     * Get the TDOP
+     */
     inline float TDOP(void) const {return 0.0;};
 
     /*! Get a pointer to the beginning of the data storage. */
@@ -41,6 +75,11 @@ public:
     /*! Return the overall data size for the structure. */
     inline static size_t DataSize(void) 
 	{return (4*sizeof(float)+16*sizeof(char));};
+
+    /*!
+     * Encode the data into a NMEA string. 
+     */
+    std::string Encode(void);
 
     /*! operator overload to output contents of class for inspection
      * this data is in character format. 
